e4945a64035e0883c7f1e5fa40381c1c036c0e5b
thompscott <tho.sco.lisd@gmail.com>
2022-08-12 09:31:50 -0500

    Hello World Displayed on Web browser

--- app.js
+++ app.js
@@ -10,3 +10,19 @@ app.use((req, res, next) => {
     console.log("App is up");
     next();
 });
+app.get('/', (req, res, next) => {
+    res.json({msg:"Hello World"});
+})
+
+// app.get('*', (req, res) => {
+//     res.status(404).send({error: '404 - Not Found', message: 'No route found for the requested URL'});
+//   });
+//   app.use((error, req, res, next) => {
+//     console.error('SERVER ERROR: ', error);
+//     if(res.statusCode < 400) {res.status(500)}
+//     res.send({error: error.error, name: error.name, message: error.message});
+//   });
+  
+  
+  
+  module.exports = app;
