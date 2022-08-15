6411a916366881c8d963050c1840fd7a2fe4b69e
Anna Mantula <ganna.mantula@gmail.com>
2022-08-15 14:41:51 -0500

    working

--- api/customers.js
+++ api/customers.js
@@ -1,5 +1,56 @@
-const express = require('express');
+const express = require("express");
 const customersRouter = express.Router();
+
+// Get all customers
+customersRouter.get("/", async (req, res, next) => {
+  try {
+    if (req.user) {
+      if (req.user.isAdmin === true) {
+        const allCustomers = await getAllContacts();
+        res.send(allCustomers);
+      } else {
+        next({
+          name: "AdministratorAccessError",
+          message: "You do not have administrator access",
+        });
+      }
+    } else {
+      next({
+        name: "UserAccessError",
+        message: "You must be logged in to perform this action",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// get customers/:customer_id
+
+customersRouter.get("/:customer_id", async (req, res, next) => {
+    const email = req.params.email
+    try {
+      if (req.user) {
+        if (req.user.isAdmin === true) {
+          const customerByEmail = await getContactByEmail(email);
+          res.send(customerByEmail);
+
+        } else {
+          next({
+            name: "AdministratorAccessError",
+            message: "You do not have administrator access",
+          });
+        }
+      } else {
+        next({
+          name: "UserAccessError",
+          message: "You must be logged in to perform this action",
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
+  });
 
 
 module.exports = customersRouter;
