e89886ac3401f2b1ccc0392cdc49ca8f8bc7d7af
thompscott <tho.sco.lisd@gmail.com>
2022-08-17 15:00:11 -0500

    cors issue

--- api/products.js
+++ api/products.js
@@ -32,7 +32,6 @@ productsRouter.get("/:product_id", async (req, res, next) => {
 });
 
 //POST product  
-//price_type???
 productsRouter.post("/", async (res, req, next) => {
   const { name, description, price, price_type, category, inventory, img_url } = req.body
   const newProduct = {};
