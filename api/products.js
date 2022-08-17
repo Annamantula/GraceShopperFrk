0be2342a8c582195fdf3a7a91dd5e10cc3ab3bb1
thompscott <tho.sco.lisd@gmail.com>
2022-08-17 16:03:01 -0500

    Running Server Locally

--- api/products.js
+++ api/products.js
@@ -32,7 +32,7 @@ productsRouter.get("/:product_id", async (req, res, next) => {
 });
 
 //POST product  
-productsRouter.post("/", async (res, req, next) => {
+productsRouter.post("/", async (req, res, next) => {
   const { name, description, price, price_type, category, inventory, img_url } = req.body
   const newProduct = {};
   try {
